00abf0c2899a84f63f11c253fb3fd969ce7379c4
Victoria <sivosha2006@mail.ru>
2021-10-22 17:54:35 +0300

    added redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { store } from "./redux";
+
 
 ReactDOM.render(
-      <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {updateLogin, updatePassword, signIn} from '../../redux/reducers/auth'
 import './Login.css'
 
 const Login = () => {
+
+  const dispatch = useDispatch()
+  const login = useSelector(s => s.auth.login)
+  const password = useSelector((s) => s.auth.password);
+
   return (
     <main>
       <section className="form-container">
         <form>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder="Username" />
+          <input
+          onChange ={(e) => {dispatch(updateLogin(e.target.value))}}
+          type="text"
+          name="username"
+          id="username"
+          value={login}
+          placeholder="Username" />
         </form>
 
         <form>
           <label htmlFor="password">Password</label>
-          <input type="text" id="password" placeholder="******************" />
+          <input
+          onChange ={(e) => {dispatch(updatePassword(e.target.value))}}
+          type="text"
+          name="password"
+          id="password"
+          value={password}
+          placeholder="******************"
+          />
         </form>
-        <button className="signin-button" type="button">
+        <button onClick={() => {dispatch(signIn())}} className="signin-button" type="button">
           Sign In
         </button>
       </section>
